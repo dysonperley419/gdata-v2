c845651f807b3d80889d014dd1815358c640bd96
dysonperley419 <perleydyson903@gmail.com>
2025-07-02 14:24:09 -0300

    Update cfg.php

--- feeds/api/cfg.php
+++ feeds/api/cfg.php
@@ -1,7 +1,7 @@
 <?php
   $apikey = "AIzaSyCTlZqLIYTLmoJRrLyj0_-dyKUopfa6o28"; 
   $lemnobase = "https://yt5.lemnoslife.com/noKey/"; //no-api gdata v3 service
-  $insturl = "yt2009.ququnta.serv00.net"; //instance url
+  $insturl = "yt2009.jawed2005.serv00.net"; //instance url
   $shema = "http"; //don't change
   $location = "RU"; //location for popular videos
  
